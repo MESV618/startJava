5ff781e82e364f04e6604f0b6f8cceb2b080fd06
Semyon Meshcheriakov <semyon618@icloud.com>
2024-02-08 20:23:51 +0300

    refactor HW RpsGameFormatting

--- lesson_1/RpsGameFormatting.java
+++ lesson_1/RpsGameFormatting.java
@@ -1,88 +1,74 @@
 import java.util.Random;
 
 public class RpsGameFormatting {
-// Игра Камень-Ножницы-Бумага
-        public static void main(String[] args) throws InterruptedException {
-        String rock ="✊";
-        String scissors= "✌\uFE0F";
-        String paper = "✋" ;
+    // Игра Камень-Ножницы-Бумага
+    public static void main(String[] args) throws InterruptedException {
+        String rock = "R";
+        String scissors = "S";
+        String paper = "P";
+        Random random = new Random();
 
+        // Ход первого игрока
+        String name1 = "HEL";
+        int position1 = random.nextInt(100) + 1;
+        String sign1 = rock;
 
-            // Ход первого игрока
-        String name1  = "HEL";
-        Random r = new Random( );
-            int position1 = r.nextInt( 1, 100 );
-            String sign1 = rock;
+        if (position1 > 66) {
+            sign1 = paper;
+        } else if (position1 > 33) {
+            sign1 = scissors;
+        }
 
+        System.out.println("Ход " + name1 + ": ");
 
-            if (position1 > 66) {
-                sign1 = paper;
-            }
-    else if (position1 > 33) {
-                sign1 = scissors;
-            }
-
-
-            System.out.println("Ход "+name1+ ": ");
-
-        for (int i = 0; i<5; i ++)
-        {
-                System.out.print (rock + "\r") ;
+        for (int i = 0; i < 5; i++) {
+            System.out.print(rock + "\r");
             Thread.sleep(100);
-                System.out.print (scissors + "\r" );
+            System.out.print(scissors + "\r");
             Thread.sleep(100);
-                System.out.print (paper + "\r");
+            System.out.print(paper + "\r");
             Thread.sleep(100);
         }
 
         System.out.println(sign1);
 
-
-    // Ход второго игрока
+        // Ход второго игрока
         String name2 = "WALLE";
-        int position2 = r.nextInt (1,100);
-            String sign2 = rock;
+        int position2 = random.nextInt(100) + 1;
+        String sign2 = rock;
 
-
-            if (position1 > 66) {
-                sign2 = paper;
-            } else {
-                if (position1 > 33) {
-                    sign2 = scissors;
-                }
-            }
-
-            System.out.println("Ход " +name2 +": ");
-
-        for (int i = 0 ; i < 5 ; i ++) {
-            System.out.print(rock + "\r");
-                Thread.sleep(100);
-            System.out.print( scissors + "\r" );
-                Thread.sleep(100);
-            System.out.print ( paper + "\r");
-                Thread.sleep(100);
+        if (position2 > 66) {
+            sign2 = paper;
+        } else if (position2 > 33) {
+            sign2 = scissors;
         }
 
-        System.out.println (sign2);
+        System.out.println("Ход " + name2 + ": ");
 
+        for (int i = 0; i < 5; i++) {
+            System.out.print(rock + "\r");
+            Thread.sleep(100);
+            System.out.print(scissors + "\r");
+            Thread.sleep(100);
+            System.out.print(paper + "\r");
+            Thread.sleep(100);
+        }
 
-        if (sign1.equals( sign2 ))
-        {
-            System.out.println("Победила дружба!") ;
+        System.out.println(sign2);
+
+        if (sign1.equals(sign2)) {
+            System.out.println("Победила дружба!");
             return;
         }
 
+        boolean isName1Winner = sign1.equals(rock) && sign2.equals(scissors) ||
+                sign1.equals(scissors) && sign2.equals(paper) ||
+                sign1.equals(paper) && sign2.equals(rock);
 
-            boolean isName1Equal = sign1.equals(rock) && sign2.equals(scissors) ||
-                    sign1.equals(scissors) && sign2.equals(paper) ||
-                    sign1.equals(paper) && sign2.equals(rock);
-
-            if (isName1Equal) {
-        System.out.println("\nПобедил - " + name1);
-          }   else {
-                System.out.println("\nПобедил - " + name2);
-            }
+        if (isName1Winner) {
+            System.out.println("\nПобедил - " + name1);
+        } else {
+            System.out.println("\nПобедил - " + name2);
+        }
     }
-
-
 }
