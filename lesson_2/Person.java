3cfae4f9be56b0e3899028ba259c89c2015b001e
Semyon Meshcheriakov <semyon618@icloud.com>
2024-02-12 20:21:51 +0300

    add hw Person

---
+++ lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String gender = "male";
+    String name = "Sam";
+    int heightCm = 190;
+    int weightKg = 97;
+    byte age = 25;
+
+    public void walk() {
+        System.out.println("walk");
+    }
+
+    public void sit() {
+        System.out.println("sit");
+    }
+
+    public void run() {
+        System.out.println("run");
+    }
+
+    public void talk() {
+        System.out.println("talk");
+    }
+
+    public void learnJava() {
+        System.out.println("learn Java");
+    }
+}
